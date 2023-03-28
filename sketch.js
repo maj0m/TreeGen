1d5bb24c53d04c3ea7e8877cccaa00ccb8eba9bb
maj0m <112952510+maj0m@users.noreply.github.com>
2023-03-28 21:56:09 +0200

    Add files via upload
    tweaked some parameters, added a button to generate a new tree

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
-var minLength = 10;
-var maxLength = 100;
+var leafSize = 15;
+var leafAlpha = 255;
+var minLength = 8;
+var maxLength = 80;
 var minThickness = 1;
-var maxThickness = 30;
-var minLengthMultiplier = 0.7;
-var maxLengthMultiplier = 0.9;
-var minRot = 10;
-var maxRot = 35;
+var maxThickness = 24;
+var minLengthMultiplier = 0.5;
+var maxLengthMultiplier = 1;
+var minRot = 20;
+var maxRot = 40;
 var maxTurns = 5;
 
+var maxTurnsSlider;
+var generateButton;
+
 function setup() {
+  generateButton = createButton("Generate Tree");
+  generateButton.position(10, 20);
+  generateButton.mousePressed(redraw);
+
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
@@ -17,7 +26,7 @@ function setup() {
 
 function draw() {
   background(100);
-  translate(width / 2, height / 2 + maxLength*2);
+  translate(width / 2, height / 2 + 200);
 
   branch(maxLength, 0, 0);
 }
@@ -42,9 +51,9 @@ function branch(len, rot, turns) {
     var r = 80 + random(-20, 20);
     var g = 120 + random(-20, 20);
     var b = 40 + random(-20, 20);
-    fill(r, g, b);
+    fill(r, g, b, leafAlpha);
     noStroke();
-    ellipse(0, 0, 15);
+    ellipse(0, 0, leafSize);
   }
 
   pop();
