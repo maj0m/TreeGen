e57f241900ffb5293b5a0d437c9e32381a4b65c3
maj0m <112952510+maj0m@users.noreply.github.com>
2023-03-30 18:23:31 +0200

    Added GUI
    Added a control panel using the dat.gui library to enable finer control over generating custom trees.

--- sketch.js
+++ sketch.js
@@ -1,60 +1,106 @@
-var leafSize = 15;
-var leafAlpha = 255;
-var minLength = 8;
-var maxLength = 80;
-var minThickness = 1;
-var maxThickness = 24;
-var minLengthMultiplier = 0.5;
-var maxLengthMultiplier = 1;
-var minRot = 20;
-var maxRot = 40;
-var maxTurns = 5;
+var leafSize;
+var leafAlpha;
+var minBranchLength;
+var maxBranchLength;
+var branchThickness;
+var leafColor;
+var branchColor;
+
+var maxTurns;
+var minLengthMultiplier;
+var maxLengthMultiplier;
+
+const minRot = 20;
+const maxRot = 40;
 
 var generateButton;
 
 function setup() {
   generateButton = createButton("Generate Tree");
   generateButton.position(10, 20);
-  generateButton.mousePressed(redraw);
+  generateButton.mousePressed(update);
 
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-
-  noLoop()
+  update();
+  noLoop();
 }
 
 function draw() {
   background(100);
-  translate(width / 2, height / 2 + 200);
-
-  branch(maxLength, 0, 0);
+  translate(width / 2, height / 2 + 250);
+  branch(maxBranchLength, 0, 0);
 }
 
 function branch(len, rot, turns) {
   push();
 
-  //branches
-  if(len > minLength && abs(turns) < maxTurns) {
+  //Branches
+  if(len > minBranchLength && abs(turns) < maxTurns) {
     rotate(rot);
-    strokeWeight(map(len, minLength, maxLength, minThickness, maxThickness));
-    stroke(70, 40, 20);
+    strokeWeight(map(len, minBranchLength, maxBranchLength, 1, branchThickness));
+    stroke(branchColor);
     line(0, 0, 0, -len);
     translate(0, -len);
     branch(len * random(minLengthMultiplier, maxLengthMultiplier), random(minRot, maxRot), turns+1);
     branch(len * random(minLengthMultiplier, maxLengthMultiplier), random(-minRot, -maxRot), turns-1);
   }
 
-  //leaves
-  else if(len <= minLength) {
-    var r = 80 + random(-20, 20);
-    var g = 120 + random(-20, 20);
-    var b = 40 + random(-20, 20);
+  //Leaves
+  else if(len <= minBranchLength) {
+    var r = leafColor[0] + random(-20, 20);
+    var g = leafColor[1] + random(-20, 20);
+    var b = leafColor[2] + random(-20, 20);
     fill(r, g, b, leafAlpha);
     noStroke();
     ellipse(0, 0, leafSize);
   }
 
   pop();
-  
+}
 
+
+
+//----------GUI----------//
+
+var controls = new function() {
+  this.leafSize = 15;
+  this.leafAlpha = 255;
+  this.minBranchLength = 8;
+  this.maxBranchLength = 80;
+  this.branchThickness = 24;
+  this.leafColor = [80, 120, 40];
+  this.branchColor = [70, 40, 20];
+  this.maxTurns = 5;
+  this.minLengthMultiplier = 0.5;
+  this.maxLengthMultiplier = 1;
+}
+
+var gui = new dat.GUI();
+gui.add(controls, 'leafSize',5,30,1);
+gui.add(controls, 'leafAlpha',0,255, 1);
+gui.add(controls, 'minBranchLength', 3, 20, 1);
+gui.add(controls, 'maxBranchLength', 50, 100, 1);
+gui.add(controls, 'branchThickness', 5, 40, 1);
+gui.addColor(controls, 'leafColor');
+gui.addColor(controls, 'branchColor');
+
+var advancedOptions = gui.addFolder('Advanced');
+advancedOptions.add(controls, 'maxTurns', 2, 20, 1);
+advancedOptions.add(controls, 'minLengthMultiplier', 0.4, 0.8, 0.01);
+advancedOptions.add(controls, 'maxLengthMultiplier', 0.8, 1, 0.01);
+
+function update() {
+  leafSize = controls.leafSize;
+  leafAlpha = controls.leafAlpha;
+  minBranchLength = controls.minBranchLength;
+  maxBranchLength = controls.maxBranchLength;
+  branchThickness = controls.branchThickness;
+  leafColor = controls.leafColor;
+  branchColor = controls.branchColor;
+  maxTurns = controls.maxTurns;
+  minLengthMultiplier = controls.minLengthMultiplier;
+  maxLengthMultiplier = controls.maxLengthMultiplier;
+  
+  redraw();
 }
